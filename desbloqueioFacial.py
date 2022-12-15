9edea3de4c4da65cb6a9121e3319243716c361b1
Samuel981 <ssoares981@gmail.com>
2022-12-15 12:01:51 -0300

    refatora codigo

--- desbloqueioFacial.py
+++ desbloqueioFacial.py
@@ -5,7 +5,6 @@
 import face_recognition # type: ignore
 import argparse # type: ignore
 import imutils # type: ignore
-import time
 import pickle
 import os
 import sys
@@ -20,64 +19,83 @@ ap.add_argument("-e", "--encodings", required=True,
                 help="path to serialized db of facial encodings")
 ap.add_argument("-i", "--input", required=True,
                 help="path to input video")
-ap.add_argument("-y", "--display", type=int, default=1,
+ap.add_argument("-d", "--display", type=int, default=1,
                 help="whether or not to display output frame to screen")
 args = vars(ap.parse_args())
 
-# aio = Client(username=access.USER, key=access.KEY)
-grupo = 'desbloqueiofacial.'
-feedTentativa = grupo+'tentativadedesbloqueio'
-feedAcesso = grupo+'sistemadeacesso'
-feedRegistro = grupo+'registro'
-feedCaptura = grupo+'captura'
+#Feeds do adafrui_io
+GRUPO = 'desbloqueiofacial.'
+FEED_TENTATIVA = GRUPO+'tentativadedesbloqueio'
+FEED_ACESSO = GRUPO+'sistemadeacesso'
+FEED_REGISTRO = GRUPO+'registro'
+FFEED_CAPTURA = GRUPO+'captura'
 
+conexao = False
 def connected(client):
-    client.subscribe(feedTentativa, access.USER)
+    client.subscribe(FEED_TENTATIVA, access.USER)
+    global conexao
+    conexao = True
     
 def disconnected(client):
-    print('Disconnected from Adafruit IO!')
+    print('[INFO] Desconectado do Adafruit IO!')
     sys.exit(1)
     
 tentativa = False
-def message(client, feed_id, payload):
+# Troca o estado da variavel com base no estado do feed
+def solicitacao(client, feed_id, payload):
     global tentativa
     tentativa = not tentativa
+    
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
 
+# Cabecalho
 os.system('cls')
 print("------- [ Sistema de desbloqueio facial ] --------\n")
 print("[INFO] Carregando encodings...")
 data = pickle.loads(open(args["encodings"], "rb").read())
+
 # Create an MQTT client instance.
-print("[INFO] Conetando com o Adafruit_IO...")
+print("[INFO] Conetando com o Adafruit_IO...", end=" ")
 client = MQTTClient(access.USER, access.KEY)
+    
 client.on_connect = connected
 client.on_disconnect = disconnected
-client.on_message = message
+client.on_message = solicitacao
+
 # Connect to the Adafruit IO server.
 client.connect()
 client.loop_background()
-stream = cv2.VideoCapture(args["input"])
-writer = None
+while not conexao:
+    blockPrint()
+    if conexao:
+        enablePrint()
+        print("conectado!")
 
-atraso = 1
+# define entrada de video e atraso entre frames
+stream = cv2.VideoCapture(args["input"])
+atraso = 50
 if stream is None:
     print("[INFO] Recebendo imagens ao vivo...")
     stream = cv2.VideoCapture(0)
-    atraso = 50
 else:
     print("[INFO] Recebendo imagens...")
     fps = round(stream.get(cv2.CAP_PROP_FPS))
     atraso = int(1000/fps) #1 segundo (1000mili) / taxa de quadros
-    
-nomeJanela = "Frame"
+
+#coloca janela em tela cheia
+nomeJanela = "Desbloqueio por reconhecimento"
 cv2.namedWindow(nomeJanela, cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty(nomeJanela, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-print("[INFO] Iniciando transmissão...")
+print("[INFO] Transmitindo...")
 
 # inicia a contagem de frames
 frame = 0
 
-# loop over frames from the video file stream
+# percorre cada frame recebido
 while True:  
     # grab the next frame
     (grabbed, frame) = stream.read()
@@ -87,13 +105,14 @@ while True:
         print("[INFO] Encerrando...")
         break
 
-    # convert the input frame from BGR to RGB
+    # convert frame from BGR to RGB
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     r = frame.shape[1] / float(rgb.shape[1])
 
     # detect the (x, y)-coordinates of the bounding boxes
     # corresponding to each face in the input frame, then compute
     # the facial embeddings for each face
+    # Se existe uma tentativa de entrada (se o botao verde foi pressionado)
     if(tentativa == True):
         boxes = face_recognition.face_locations(rgb, model='hog')
         encodings = face_recognition.face_encodings(rgb, boxes)
@@ -101,8 +120,7 @@ while True:
 
         # loop over the facial embeddings
         for encoding in encodings:
-            # attempt to match each face in the input image to our known
-            # encodings
+            # percorre banco de dados para encontrar correspondencia
             matches = face_recognition.compare_faces(data["encodings"],
                                                     encoding)
             name = "Desconhecido"
@@ -146,31 +164,26 @@ while True:
                         0.75, (0, 255, 0), 2)
             
             if(name != "Desconhecido"):
-                client.publish(feedAcesso, "ON")
-                client.publish(feedRegistro, "Desbloqueado por "+name)
+                client.publish(FEED_ACESSO, "ON")
+                client.publish(FEED_REGISTRO, "Desbloqueado por "+name)
             else:
-                client.publish(feedRegistro, "Tentativa de desbloqueio por <desconhecido>")
+                client.publish(FEED_REGISTRO, "Tentativa de desbloqueio por <desconhecido>")
                 dataAtual = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
                 cv2.imwrite("images/nao_autorizado_"+dataAtual+".jpg", frame)
                 with open("images/nao_autorizado_"+dataAtual+".jpg", 'rb') as imageFile:
                     img = b64encode(imageFile.read())
-                    client.publish(feedCaptura,  img.decode('utf-8'))
+                    client.publish(FFEED_CAPTURA,  img.decode('utf-8'))
                 imageFile.close()
-            client.publish(feedTentativa, "False")
+            client.publish(FEED_TENTATIVA, "False")
             # tentativa = False
 
-    # check to see if we are supposed to display the output frame to
-    # the screen
+    # controla exibicao
     if args["display"] > 0:
         cv2.imshow(nomeJanela, frame)
         key = cv2.waitKey(atraso) & 0xFF
 
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
+        # aperter 'q' ou [esc] para sair
+        if key == ord("q") or key == 27:
             print("[INFO] Encerrando...")
-            break
+            disconnected(client)
     frame += 1
-
-# check to see if the video writer point needs to be released
-if writer is not None:
-    writer.release()
